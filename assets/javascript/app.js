e8aa3399ecd856467cb63c81b56d94d7e7a5647e
Kevin Beeler <kbeeler07@gmail.com>
2018-03-02 23:23:51 -0500

    first HW push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,40 @@
 //page loads
 $( document ).ready(function() {
     $('#startOver').hide();
+    $('#done').hide();
+    $(".stats").hide();
 
 });
+
+ 
+ 
+ //global variables
+ // set counter to 120 sec 
+ var number = 120;
+ var intervalId
+  
+  //trivia questionsArr array
+ var questionsArr = [{
+   question: "What cartoon is full of talking babies ?",
+   choices: ["doug","Rugrats", "cat-dog", "sonic"],
+   correctAnswer: 1
+ }, {
+   question: "What cartoon has two animals joined at the hip ?",
+   choices: ["Rugrats", "Sonic", "Angry Beavers","cat-dog",],
+   correctAnswer: 3
+ }, {
+   question: "A kids has a head shapped like a football ?",
+   choices: ["Doug", "Sailor Moon", "Power Puffs","Hey Arnold"],
+   correctAnswer: 3
+ }, {
+   question: "In what cartoon is the sister's name DEE DEE ?",
+   choices: ["Dexter's Lab","Ed, Ed, & Eddy", "Johnny Bravo", "Angry Beavers"],
+   correctAnswer: 0
+ }, {
+   question: "What cartoon is about taking over the world ? ",
+   choices: ["Dexter's lab","Rugrats","Pinky and the brain","Spongbob"],
+   correctAnswer: 2
+ }];
 
  //click handler -press start to begin the game
  $("#start").on("click", function(event) {
@@ -12,13 +44,8 @@ $( document ).ready(function() {
    timer();
    // display Question
    nextQuestion();
+   $('#done').show()
  });
- 
- //global variables
- // set counter to 120 sec 
- var number = 120;
- var intervalId
-  //  Decrease number by one.
 
 //set interval
 function timer() {
@@ -40,59 +67,27 @@ function stop() {
  clearInterval(intervalId);
 }
 
-//trivia questions array
- var questions = [{
-   question: "How old am I- ",
-   choices: [12, 14, 18, 19],
-   correctAnswer: 3
- }, {
-   question: "What color is my hair- ",
-   choices: ["brown", "red", "green", "black"],
-   correctAnswer: 0
- }, {
-   question: "What size shoe do I wear- ",
-   choices: [5, 6, 7, 8],
-   correctAnswer: 2
- }, {
-   question: "Where was I born- ",
-   choices: ["cleveland", "fremont", "NYC", "florida"],
-   correctAnswer: 2
- }, {
-   question: "How many siblings do I have- ",
-   choices: [1, 2, 3, 4],
-   correctAnswer: 3
- }];
-
-
 // var question = $("#question")
 // var options = $("#options")
 
 
 function nextQuestion() {
- // $("#question").append(questions.choices)
+ // $("#question").append(questionsArr.choices)
  console.log(nextQuestion);
-   for (var i = 0; i < questions.length; i++) {
+   for (var i = 0; i < questionsArr.length; i++) {
      var div = $("<div class= 'question'>" );
-     div.text(questions[i].question);
+     div.text(questionsArr[i].question);
     $("#question").append(div);
-    // $("#question").append(questions[i].choices);
+    // $("#question").append(questionsArr[i].choices);
     addAnswers();
 
       };
 
-    function addAnswers(answers) {
-          var radioList = $('<ul>');
-          var item;
-          var button = '';
-          for (var j = 0; j < questions[j].choices.length; j++) { 
-                item = $('<li>');
-                button = $('<input type ="radio" name= "choices" value=' + i + '/>');
-                button.html(questions[i].choices[j]);
-                item.append(button);
-                radioList.append(item);
-                
-
-                if (j === questions[i].correctAnswer) {
+      function addAnswers(answers) {
+          for (var j = 0; j < questionsArr[j].choices.length; j++) {          
+                var button = $("<button class= 'choices'>");
+                button.text(questionsArr[i].choices[j]);
+                if (j === questionsArr[i].correctAnswer) {
                  button.attr('data-value', 'correct');
                 }  
                 else {
@@ -101,27 +96,83 @@ function nextQuestion() {
                 $("#question").append(button);
 
            };
-           return radioList;
        };
 
+   };
  
-};
+
+var done= $(".done")
 
 //track wins and losses
 var win = 0
+docWins = $(".win")
+docWins.textContent = win;
+
 var lose = 0
+docWins = $(".lose")
+docWins.textContent = lose;
+
+
 
 $(document).on("click", '.choices', function() {
  if(this.getAttribute('data-value') === 'correct') {
    win++;
-   console.log(win);
+   console.log('win');
  }
  else if(this.getAttribute('data-value') === 'wrong') {
    lose++;
-   console.log(lose);
+   console.log('lose');
  }
+ $(".win")
+ //top button 
+
+ $("#done").on("click", function() {
+   //hide start after clicked
+   $('#done').hide();
+   $('.choices').hide();
+   $('.question').hide();
+   $('.stats').show();
+   stop();
+   $('#startOver').show();
+   $('#countDown').hide();
  
+ });
+ 
+ $("#startOver").on("click", function() {
+   //hide start after clicked
+   timer();
+   $('#done').show();
+   $('.choices').show();
+   $('.question').show();
+   $('.stats').hide();
+   $('#startOver').hide();
+   $('#countDown').show();
+ });
+
  //add if else conditions to check if data value = wrong or right answer
+
+// trying to create a done button 
+//stop timer. 
+//clear page.
+//display wins and loss 
+
+//  var button = $("<button>done</button>");
+ 
+//  $("#done_div").append(button);
+//  button.button();
+
+// $("#done1").on("click", function() {
+ 
+//  stop();
+//   }
+
+  //store userguees when selected 
+  //make a done button
+  // make a status page 
+
+
+
+
 });
 
   
